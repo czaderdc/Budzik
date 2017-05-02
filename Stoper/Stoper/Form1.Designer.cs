e8b0a1bb097babb4cf9ad73e078c44f92c194fbe
czaderdc <konrad0spoko@gmail.com>
2017-05-02 13:32:10 +0200

    Budzik

---
+++ Stoper/Stoper/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Stoper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.circularProgressBar1 = new CircularProgressBar.CircularProgressBar();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // circularProgressBar1
+            // 
+            this.circularProgressBar1.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner;
+            this.circularProgressBar1.AnimationSpeed = 1000;
+            this.circularProgressBar1.BackColor = System.Drawing.Color.Transparent;
+            this.circularProgressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.circularProgressBar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold);
+            this.circularProgressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.circularProgressBar1.InnerColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.circularProgressBar1.InnerMargin = 2;
+            this.circularProgressBar1.InnerWidth = -1;
+            this.circularProgressBar1.Location = new System.Drawing.Point(0, 0);
+            this.circularProgressBar1.MarqueeAnimationSpeed = 2000;
+            this.circularProgressBar1.Name = "circularProgressBar1";
+            this.circularProgressBar1.OuterColor = System.Drawing.Color.Gray;
+            this.circularProgressBar1.OuterMargin = -25;
+            this.circularProgressBar1.OuterWidth = 26;
+            this.circularProgressBar1.ProgressColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.circularProgressBar1.ProgressWidth = 25;
+            this.circularProgressBar1.SecondaryFont = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.circularProgressBar1.Size = new System.Drawing.Size(604, 559);
+            this.circularProgressBar1.StartAngle = 270;
+            this.circularProgressBar1.SubscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+            this.circularProgressBar1.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
+            this.circularProgressBar1.SubscriptText = "00";
+            this.circularProgressBar1.SuperscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+            this.circularProgressBar1.SuperscriptMargin = new System.Windows.Forms.Padding(10, 35, 0, 0);
+            this.circularProgressBar1.SuperscriptText = "";
+            this.circularProgressBar1.TabIndex = 0;
+            this.circularProgressBar1.Text = "00:00:00";
+            this.circularProgressBar1.TextMargin = new System.Windows.Forms.Padding(8, 8, 0, 0);
+            this.circularProgressBar1.Value = 68;
+            this.circularProgressBar1.Click += new System.EventHandler(this.circularProgressBar1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ustaw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(472, 509);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "WybierzDzwonek";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 559);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.circularProgressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CircularProgressBar.CircularProgressBar circularProgressBar1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Stoper/Stoper/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+using System.Threading;
+using System.Media;
+
+namespace Stoper
+{
+    public partial class Form1 : Form
+    {
+        private SoundPlayer soundPlayer;
+        string wybranaGodzina = "";
+        string lokacjaPiosenki = "";
+        Thread a;
+        System.Timers.Timer timer;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void circularProgressBar1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            soundPlayer = new SoundPlayer();
+            this.dateTimePicker1.CustomFormat = "HH:mm";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.ShowUpDown = true;
+            timer = new System.Timers.Timer();
+            timer.Interval = 1000;
+          //  dateTimePicker1.TextChanged += ZmianaGodziny;
+            timer.Elapsed += AktualizujProgressBar;
+            timer.Start();
+        }
+
+       
+
+        //private void ZmianaGodziny(object sender, EventArgs e)
+        //{
+        //    string minuty = dateTimePicker1.Value.Minute < 10 ? (0.ToString() + dateTimePicker1.Value.Minute).ToString() : dateTimePicker1.Value.Minute.ToString();
+        //    wybranaGodzina = dateTimePicker1.Value.Hour.ToString() +":" + minuty;
+        //    MessageBox.Show("Wybrana godzina: " + wybranaGodzina);
+        //}
+
+        private void AktualizujProgressBar(object sender, ElapsedEventArgs e)
+        {
+            
+            circularProgressBar1.Invoke(
+             (MethodInvoker)delegate
+                            {
+                            circularProgressBar1.Text = DateTime.Now.ToString("HH:mm:ss");
+                            //circularProgressBar1.SuperscriptText = DateTime.Now.ToString("tt");
+                                if (wybranaGodzina == DateTime.Now.ToString("HH:mm"))
+                                {
+                                  //  MessageBox.Show("TA");
+                                    a = new Thread(WlaczBudzik);
+                                    a.Start();
+                                   wybranaGodzina = "";//resetuje godzine aby event ciagle nie wywolywal metody WlaczBudzik
+
+                                }
+                            });
+
+        }
+
+        private void WlaczBudzik()
+        {
+            soundPlayer.PlayLooping();
+
+        }
+
+  
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(lokacjaPiosenki))
+            {
+                MessageBox.Show("Musisz wybrać dzwięk budzika! Dostępne tylko pliki .wav");
+                return;
+            }
+            string minuty = dateTimePicker1.Value.Minute < 10 ? (0.ToString() + dateTimePicker1.Value.Minute).ToString() : dateTimePicker1.Value.Minute.ToString();
+            wybranaGodzina = dateTimePicker1.Value.Hour.ToString() +":" + minuty;
+            MessageBox.Show("Budzik włączy się o:  " + wybranaGodzina);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.CheckFileExists = true;
+
+            ofd.Filter = "WAV files (*.wav)|*.wav";
+            ofd.DefaultExt = ".wav";
+
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                lokacjaPiosenki = ofd.FileName;
+
+                soundPlayer.SoundLocation = lokacjaPiosenki;
+            }
+        }
+    }
+}
